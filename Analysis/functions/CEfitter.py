bf25b58599bddae040d752c0265aedd13eb6ec86
lzampieri <zampieri.leonardo98@gmail.com>
2022-08-18 17:52:30 +0200

    Update

--- Analysis/functions/CEfitter.py
+++ Analysis/functions/CEfitter.py
@@ -3,6 +3,7 @@ from re import L
 import numpy as np
 from uncertainties import *
 from uncertainties import unumpy as unp
+from functions import splt
 
 def is_straight( logx, y, B, Bmax ):
     if( B >= Bmax ):
@@ -72,19 +73,49 @@ def estimate_B_errony( x, y, plot = False ):
     BabsMax = np.min( y_n )
     cont = 0
 
-    while( Bmax - Bmin > 0.00000001 * BabsMax ):
+    while( True ):
         Bs = np.linspace( Bmin, Bmax, 500 )
-        res = [ is_straight( logx, y_n, B, BabsMax ) for B in Bs ]
-        if( plot ):
-            plt.semilogx( Bs, res, '.b' )
-        i = np.argmin( res )
-        Bmin = Bs[ max( 0, np.min( i ) - 2 ) ]
-        Bmax = Bs[ min( len( Bs ) - 1, np.max( i ) + 2 ) ]
+        res = np.array( [ is_straight( logx, y_n, B, BabsMax ) for B in Bs ] )
+        res[ res == np.inf ] = np.max( res[ res < np.inf ] ) # remove infinite
+
+        i = np.where( res < np.max( res ) - 0.9 * np.ptp( res ) )[0]
+        if( np.ptp( res ) == 0 ):
+            break
+
+        imin = np.min( i )
+        imax = np.max( i )
+
+        imin = np.max( [0, np.min( [ imin, imax - 3 ]) ] )
+        imax = np.min( [len( Bs ) - 1, np.max( [ imax, imin + 3 ]) ] )
+
+        Bmin = Bs[ imin ]
+        Bmax = Bs[ imax ]
+
         cont = cont + 1
 
-    B = ufloat( np.mean( [Bmin, Bmax ] ), ( Bmax - Bmin ) / np.sqrt( 12 ) )
+        if( cont > 3 ): # Make at least 3 steps
+            x_simplified = np.arange( len( Bs ) )
+            p = np.polyfit( x_simplified, res, 2 )
+            res_expected = p[0] * ( x_simplified**2 ) + p[1] * x_simplified + p[2]
+            
+            if( np.std( res - res_expected ) * 2 > np.ptp( res_expected ) ):
+                break
+            if( cont > 30 ):
+                break
+
+        old_bs = Bs
+        old_res = res
+
+
     if( plot ):
-        plt.plot( B.n, is_straight( logx, y_n, B.n, BabsMax ), 'rs' )
+        print( cont )
+        plt.plot( x_simplified, old_res, label = cont - 1 )
+        plt.plot( x_simplified, res, label = cont )
+        plt.plot( x_simplified, res_expected )
+        plt.legend()
+        plt.ylim( np.min( old_res ), np.max( old_res ) )
+
+    B = ufloat( np.mean( [Bmin, Bmax ] ), ( Bmax - Bmin ) )
     logyB = unp.uarray(
         np.log2( np.array( y_n ) - B.n ),
         np.sqrt( B.s **2 + unp.std_devs( y ) **2 ) / np.abs( B.n - np.array( y_n ) ) / np.log( 2 )
