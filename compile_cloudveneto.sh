af1f94ab73e99f8e51b1e896989249f761edc086
lzampieri <zampieri.leonardo98@gmail.com>
2022-10-16 13:08:28 +0200

    Typo

--- compile_cloudveneto.sh
+++ compile_cloudveneto.sh
@@ -1,1 +1,1 @@
-rm -f NumericS/main && g++-11 *.cpp -o NumericS/main -std=c++2a -lfftw3 -lm -lgsl -lgslcblas -pthread -march=native -flto -frename-registers -funroll-loops
+rm -f NumericS/main && g++-11 src/*.cpp -o NumericS/main -std=c++2a -lfftw3 -lm -lgsl -lgslcblas -pthread -march=native -flto -frename-registers -funroll-loops
